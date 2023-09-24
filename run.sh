5c62aa9320f89df129504b35cde3678a709f8039
Carlos Fuentealba <carlos@fuentealba.dev>
2023-09-24 08:13:04 -0300

    Update run.sh
    se incorporan DNS de Telefonica Chile en la invocación del contenedor para forzar la ejecución del script en el contenedor

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --rm -w /tmp -v /home/soporte/speedtest-online:/tmp -h $(hostname) cfuentealba/speedtest:latest python3 prueba.py
+docker run --rm -w /tmp -v /home/soporte/speedtest-online:/tmp -h $(hostname) --dns 200.28.4.130 --dns 200.28.4.129 cfuentealba/speedtest:latest python3 prueba.py
